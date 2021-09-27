d2222147e8222528750420cf3accb4221404d5a1
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-09-27 16:35:01 +0900

    Update Lit.Test

--- build/test/__snapshots__/TodoTest.snap.js
+++ build/test/__snapshots__/TodoTest.snap.js
@@ -2,10 +2,148 @@
 export const snapshots = {};
 
 snapshots["before new todo"] = 
-`%3Cdiv%3E%0A%20%20%20%20%3Cdiv%20style%3D%22margin%3A%200%20auto%3B%20max-width%3A%20800px%3B%20padding%3A%2020px%3B%22%3E%0A%20%20%20%20%3Cp%20class%3D%22title%22%3ELit.TodoMVC%3C%2Fp%3E%0A%20%20%20%20%3Cdiv%20class%3D%22field%20has-addons%22%3E%0A%20%20%20%20%3Cdiv%20class%3D%22control%20is-expanded%22%3E%0A%20%20%20%20%3Cinput%20type%3D%22text%22%20class%3D%22input%20is-medium%22%20aria-label%3D%22New%20todo%20description%22%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3Cdiv%20class%3D%22control%22%3E%0A%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%20is-medium%22%20aria-label%3D%22Add%20new%20todo%22%3E%0A%20%20%20%20%3Ci%20class%3D%22fa%20fa-plus%22%3E%3C%2Fi%3E%0A%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22columns%22%20style%3D%22transition-duration%3A%20500ms%3B%20opacity%3A%200%3B%20transform%3A%20scale(2)%3B%0A%20%20%20%20%20%20%20%20border%3A%202px%20solid%20lightgray%3B%0A%20%20%20%20%20%20%20%20border-radius%3A%2010px%3B%0A%20%20%20%20%20%20%20%20margin%3A%205px%200%3B%0A%20%20%20%20%22%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-9%22%3E%0A%20%20%20%20%20%20%20%20%3Cp%20class%3D%22subtitle%22%20style%3D%22cursor%3A%20pointer%3B%20user-select%3A%20none%3B%22%3E%0A%20%20%20%20%20%20%20%20Learn%20F%23%0A%20%20%20%20%20%20%20%20%3C%2Fp%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-3%22%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%22%20aria-label%3D%22Mark%20completed%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-check%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%22%20aria-label%3D%22Edit%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-edit%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-danger%22%20aria-label%3D%22Delete%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-times%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22columns%22%20style%3D%22transition-duration%3A%20500ms%3B%20opacity%3A%200%3B%20transform%3A%20scale(2)%3B%0A%20%20%20%20%20%20%20%20border%3A%202px%20solid%20lightgray%3B%0A%20%20%20%20%20%20%20%20border-radius%3A%2010px%3B%0A%20%20%20%20%20%20%20%20margin%3A%205px%200%3B%0A%20%20%20%20%22%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-9%22%3E%0A%20%20%20%20%20%20%20%20%3Cp%20class%3D%22subtitle%22%20style%3D%22cursor%3A%20pointer%3B%20user-select%3A%20none%3B%22%3E%0A%20%20%20%20%20%20%20%20Have%20fun%20with%20Lit!%0A%20%20%20%20%20%20%20%20%3C%2Fp%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-3%22%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%22%20aria-label%3D%22Mark%20completed%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-check%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%22%20aria-label%3D%22Edit%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-edit%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-danger%22%20aria-label%3D%22Delete%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-times%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3C%2Fdiv%3E`;
+`<div>
+    <div style="margin: 0 auto; max-width: 800px; padding: 20px;">
+    <p class="title">Lit.TodoMVC</p>
+    <div class="field has-addons">
+    <div class="control is-expanded">
+    <input type="text" class="input is-medium" aria-label="New todo description">
+    </div>
+    <div class="control">
+    <button class="button is-primary is-medium" aria-label="Add new todo">
+    <i class="fa fa-plus"></i>
+    </button>
+    </div>
+    </div>
+        <div class="columns" style="transition-duration: 500ms; opacity: 0; transform: scale(2);
+        border: 2px solid lightgray;
+        border-radius: 10px;
+        margin: 5px 0;
+    ">
+        <div class="column is-9">
+        <p class="subtitle" style="cursor: pointer; user-select: none;">
+        Learn F#
+        </p>
+        </div>
+        <div class="column is-3">
+        <button class="button" aria-label="Mark completed">
+        <i class="fa fa-check"></i>
+        </button>
+        <button class="button is-primary" aria-label="Edit">
+        <i class="fa fa-edit"></i>
+        </button>
+        <button class="button is-danger" aria-label="Delete">
+        <i class="fa fa-times"></i>
+        </button>
+        </div>
+        </div>
+        <div class="columns" style="transition-duration: 500ms; opacity: 0; transform: scale(2);
+        border: 2px solid lightgray;
+        border-radius: 10px;
+        margin: 5px 0;
+    ">
+        <div class="column is-9">
+        <p class="subtitle" style="cursor: pointer; user-select: none;">
+        Have fun with Lit!
+        </p>
+        </div>
+        <div class="column is-3">
+        <button class="button" aria-label="Mark completed">
+        <i class="fa fa-check"></i>
+        </button>
+        <button class="button is-primary" aria-label="Edit">
+        <i class="fa fa-edit"></i>
+        </button>
+        <button class="button is-danger" aria-label="Delete">
+        <i class="fa fa-times"></i>
+        </button>
+        </div>
+        </div>
+    </div>
+    </div>`;
 /* end snapshot before new todo */
 
 snapshots["after new todo"] = 
-`%3Cdiv%3E%0A%20%20%20%20%3Cdiv%20style%3D%22margin%3A%200%20auto%3B%20max-width%3A%20800px%3B%20padding%3A%2020px%3B%22%3E%0A%20%20%20%20%3Cp%20class%3D%22title%22%3ELit.TodoMVC%3C%2Fp%3E%0A%20%20%20%20%3Cdiv%20class%3D%22field%20has-addons%22%3E%0A%20%20%20%20%3Cdiv%20class%3D%22control%20is-expanded%22%3E%0A%20%20%20%20%3Cinput%20type%3D%22text%22%20class%3D%22input%20is-medium%22%20aria-label%3D%22New%20todo%20description%22%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3Cdiv%20class%3D%22control%22%3E%0A%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%20is-medium%22%20aria-label%3D%22Add%20new%20todo%22%3E%0A%20%20%20%20%3Ci%20class%3D%22fa%20fa-plus%22%3E%3C%2Fi%3E%0A%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22columns%22%20style%3D%22transition-duration%3A%20500ms%3B%0A%20%20%20%20%20%20%20%20border%3A%202px%20solid%20lightgray%3B%0A%20%20%20%20%20%20%20%20border-radius%3A%2010px%3B%0A%20%20%20%20%20%20%20%20margin%3A%205px%200%3B%0A%20%20%20%20%22%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-9%22%3E%0A%20%20%20%20%20%20%20%20%3Cp%20class%3D%22subtitle%22%20style%3D%22cursor%3A%20pointer%3B%20user-select%3A%20none%3B%22%3E%0A%20%20%20%20%20%20%20%20Todo%20test%0A%20%20%20%20%20%20%20%20%3C%2Fp%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-3%22%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%22%20aria-label%3D%22Mark%20completed%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-check%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%22%20aria-label%3D%22Edit%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-edit%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-danger%22%20aria-label%3D%22Delete%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-times%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22columns%22%20style%3D%22transition-duration%3A%20500ms%3B%0A%20%20%20%20%20%20%20%20border%3A%202px%20solid%20lightgray%3B%0A%20%20%20%20%20%20%20%20border-radius%3A%2010px%3B%0A%20%20%20%20%20%20%20%20margin%3A%205px%200%3B%0A%20%20%20%20%22%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-9%22%3E%0A%20%20%20%20%20%20%20%20%3Cp%20class%3D%22subtitle%22%20style%3D%22cursor%3A%20pointer%3B%20user-select%3A%20none%3B%22%3E%0A%20%20%20%20%20%20%20%20Learn%20F%23%0A%20%20%20%20%20%20%20%20%3C%2Fp%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-3%22%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%22%20aria-label%3D%22Mark%20completed%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-check%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%22%20aria-label%3D%22Edit%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-edit%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-danger%22%20aria-label%3D%22Delete%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-times%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22columns%22%20style%3D%22transition-duration%3A%20500ms%3B%0A%20%20%20%20%20%20%20%20border%3A%202px%20solid%20lightgray%3B%0A%20%20%20%20%20%20%20%20border-radius%3A%2010px%3B%0A%20%20%20%20%20%20%20%20margin%3A%205px%200%3B%0A%20%20%20%20%22%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-9%22%3E%0A%20%20%20%20%20%20%20%20%3Cp%20class%3D%22subtitle%22%20style%3D%22cursor%3A%20pointer%3B%20user-select%3A%20none%3B%22%3E%0A%20%20%20%20%20%20%20%20Have%20fun%20with%20Lit!%0A%20%20%20%20%20%20%20%20%3C%2Fp%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3Cdiv%20class%3D%22column%20is-3%22%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%22%20aria-label%3D%22Mark%20completed%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-check%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-primary%22%20aria-label%3D%22Edit%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-edit%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3Cbutton%20class%3D%22button%20is-danger%22%20aria-label%3D%22Delete%22%3E%0A%20%20%20%20%20%20%20%20%3Ci%20class%3D%22fa%20fa-times%22%3E%3C%2Fi%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20%3C%2Fdiv%3E`;
+`<div>
+    <div style="margin: 0 auto; max-width: 800px; padding: 20px;">
+    <p class="title">Lit.TodoMVC</p>
+    <div class="field has-addons">
+    <div class="control is-expanded">
+    <input type="text" class="input is-medium" aria-label="New todo description">
+    </div>
+    <div class="control">
+    <button class="button is-primary is-medium" aria-label="Add new todo">
+    <i class="fa fa-plus"></i>
+    </button>
+    </div>
+    </div>
+        <div class="columns" style="transition-duration: 500ms; opacity: 0; transform: scale(2);
+        border: 2px solid lightgray;
+        border-radius: 10px;
+        margin: 5px 0;
+    ">
+        <div class="column is-9">
+        <p class="subtitle" style="cursor: pointer; user-select: none;">
+        Elmish test
+        </p>
+        </div>
+        <div class="column is-3">
+        <button class="button" aria-label="Mark completed">
+        <i class="fa fa-check"></i>
+        </button>
+        <button class="button is-primary" aria-label="Edit">
+        <i class="fa fa-edit"></i>
+        </button>
+        <button class="button is-danger" aria-label="Delete">
+        <i class="fa fa-times"></i>
+        </button>
+        </div>
+        </div>
+        <div class="columns" style="transition-duration: 500ms;
+        border: 2px solid lightgray;
+        border-radius: 10px;
+        margin: 5px 0;
+    ">
+        <div class="column is-9">
+        <p class="subtitle" style="cursor: pointer; user-select: none;">
+        Learn F#
+        </p>
+        </div>
+        <div class="column is-3">
+        <button class="button" aria-label="Mark completed">
+        <i class="fa fa-check"></i>
+        </button>
+        <button class="button is-primary" aria-label="Edit">
+        <i class="fa fa-edit"></i>
+        </button>
+        <button class="button is-danger" aria-label="Delete">
+        <i class="fa fa-times"></i>
+        </button>
+        </div>
+        </div>
+        <div class="columns" style="transition-duration: 500ms;
+        border: 2px solid lightgray;
+        border-radius: 10px;
+        margin: 5px 0;
+    ">
+        <div class="column is-9">
+        <p class="subtitle" style="cursor: pointer; user-select: none;">
+        Have fun with Lit!
+        </p>
+        </div>
+        <div class="column is-3">
+        <button class="button" aria-label="Mark completed">
+        <i class="fa fa-check"></i>
+        </button>
+        <button class="button is-primary" aria-label="Edit">
+        <i class="fa fa-edit"></i>
+        </button>
+        <button class="button is-danger" aria-label="Delete">
+        <i class="fa fa-times"></i>
+        </button>
+        </div>
+        </div>
+    </div>
+    </div>`;
 /* end snapshot after new todo */
 
--- test/TodoTest.fs
+++ test/TodoTest.fs
@@ -9,20 +9,26 @@ open Lit.TodoMVC.App
 
 describe "Todo" <| fun () ->
     it "New todo" <| fun () -> promise {
-        let! el, obs =
+        // Initialize the Elmish app with Program.runTest, this will return a container
+        // that's also an observable reporting model updates
+        use! container =
             Program.mkProgram init update view
             |> Program.runTest
 
-        let el = el.El
+        // Access the element from the container and do snapshot testing
+        let el = container.El
         do! el |> Expect.matchHtmlSnapshot "before new todo"
 
-        el.getTextInput("New todo description").value <- "Todo test"
+        // We can get the form elements using the aria labels, same way as screen readers will do
+        el.getTextInput("New todo description").value <- "Elmish test"
         el.getButton("Add new todo").click()
 
-        let! model = obs.Await()
-        let newTodo = model.Todos |> List.find (fun t -> t.Description = "Todo test")
+        // Get the updated model and confirm it contains the new todo uncompleted
+        let! model = container.Await()
+        let newTodo = model.Todos |> List.find (fun t -> t.Description = "Elmish test")
         newTodo |> Expect.isFalse "new todo complete" (fun t -> t.Completed)
 
+        // Await for the element to update
         do! elementUpdated el
         do! el |> Expect.matchHtmlSnapshot "after new todo"
     }
